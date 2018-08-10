3409ecc7675c3b22378d67ff7862e63959ca35c7
René Ziegler <ziegler.rene87@gmx.de>
2018-08-10 14:04:19 +0200

    Did some refactoring for todolists QuestionnaireView can for now be reached via "Anwaltssuche" from the NavigationDrawer

---
+++ js/todoList.js
@@ -0,0 +1,96 @@
+class TodoListEntry {
+    constructor(title, text, state) {
+        this.id = guid();
+        this.title = title;
+        this.text = text;
+        this.state = state;
+    }
+}
+
+class TodoList {
+    constructor() {
+        this.entries = []
+    }
+
+    add(entry) {
+        this.entries.push(entry);
+    }
+
+    removeEntryById(id) {
+        const result = this.entries.filter(e => e.id === id);
+
+        if(result.length > 0) {
+            let i = this.entries.indexOf(result[0]);
+
+            if(i !== -1) {
+                this.entries.splice(i, 1);
+            }
+        }
+    }
+
+    getEntryById(id) {
+        return this.entries.filter(e => e.id === id)[0];
+    }
+
+    moveEntryUpById(id) {
+        let entry = this.getEntryById(id);
+        if(entry === undefined) return false;
+
+        let pos = this.entries.indexOf(entry);
+        let newPos = pos-1;
+
+        if(newPos >= 0) {
+            TodoList.array_move(this.entries, pos, newPos);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    moveEntryDownById(id) {
+        let entry = this.getEntryById(id);
+        if(entry === undefined) return false;
+
+        let pos = this.entries.indexOf(entry);
+        let newPos = pos+1;
+
+        if(newPos < this.entries.length) {
+            TodoList.array_move(this.entries, pos, newPos);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    static array_move(arr, old_index, new_index) {
+        if (new_index >= arr.length) {
+            let k = new_index - arr.length + 1;
+            while (k--) {
+                arr.push(undefined);
+            }
+        }
+        arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+        return arr;
+    }
+}
+
+function storeTodoListData(key) {
+    console.log("Storing TodoListData for key " + key);
+
+    let json = JSON.stringify(myTodoList);
+    localStorage.setItem(key, json);
+}
+
+function loadTodoListData(key) {
+    let json = localStorage.getItem(key);
+    let jsonObject = JSON.parse(json);
+    let todoList = Object.assign(new TodoList, jsonObject);
+
+    if(todoList === null) {
+        todoList = new TodoList();
+    }
+
+    return todoList;
+}
--- js/todoListHandler.js
+++ js/todoListHandler.js
@@ -1,11 +1,7 @@
-var todoList;
+var myTodoList;
 var listEntryTemplate = '<li class="mdl-list__item mdl-list__item--two-line"> ' +
-    '<button class="mdl-button mdl-js-button mdl-button--icon mdl-ext__micro-button">'+
-    '<i class="material-icons">keyboard_arrow_up</i>'+
-    '</button>'+
-    '<button class="mdl-button mdl-js-button mdl-button--icon mdl-ext__micro-button">'+
-    '<i class="material-icons">keyboard_arrow_down</i>'+
-    '</button>'+
+    '{$uparrowbutton}'+
+    '{$downarrowbutton}'+
     '<span class="mdl-list__item-primary-content"> ' +
     '<span>{$title}</span> ' +
     '<span class="mdl-list__item-sub-title">{$text}</span> ' +
@@ -14,8 +10,16 @@ var listEntryTemplate = '<li class="mdl-list__item mdl-list__item--two-line"> ' 
     '<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="{$id}"> ' +
     '<input type="checkbox" id="{$id}" class="mdl-checkbox__input" {$state}/> </label> </span> </li>';
 
+var upArrowButtonTemplate = '<button class="mdl-button mdl-js-button mdl-button--icon mdl-ext__micro-button">'+
+    '<i class="material-icons">keyboard_arrow_up</i>'+
+    '</button>';
+
+var downArrowButtonTemplate = '<button class="mdl-button mdl-js-button mdl-button--icon mdl-ext__micro-button">'+
+    '<i class="material-icons">keyboard_arrow_down</i>'+
+    '</button>';
+
 function initTestData() {
-    todoList = new TodoList();
+    myTodoList = new TodoList();
 
     createNewEntry("a", "lorem ipsum dolor sit amet");
     createNewEntry("b", "lorem ipsum dolor sit amet");
@@ -24,7 +28,7 @@ function initTestData() {
 }
 
 function initTodoListHandler() {
-    loadEntriesFromStorage();
+    loadMyTodoListEntriesFromStorage();
 
     initDeleteConfirmDialog();
     initAddEntryDialog();
@@ -36,8 +40,8 @@ function initProgressbar() {
 }
 
 function updateProgressbar() {
-    let entries = todoList.entries.length;
-    let done = todoList.entries.filter(e => e.state).length;
+    let entries = myTodoList.entries.length;
+    let done = myTodoList.entries.filter(e => e.state).length;
     let val = (done / entries) * 100;
 
     $("#myTodoProgressbar>.progressbar").css("width", val + "%");
@@ -99,38 +103,41 @@ function initDeleteConfirmDialog() {
         });
 }
 
-function loadEntriesFromStorage() {
-    loadTodoListData();
-
-    cleanupEntryHtml();
-    todoList.entries.forEach(entry => {
-        appendEntryHtml(entry);
-    })
+function loadMyTodoListEntriesFromStorage() {
+    myTodoList = loadTodoListData("myTodo");
+    cleanupEntryHtml("myTodoList");
+    myTodoList.entries.forEach(entry => {
+        appendEntryHtml(entry, "myTodoList");
+    });
 }
 
 function handleChangedTodoListData() {
-    storeTodoListData();
+    storeTodoListData("myTodo");
     updateProgressbar();
 }
 
 function createNewEntry(title, text) {
     const entry = new TodoListEntry(title, text, false);
 
-    todoList.add(entry);
-    appendEntryHtml(entry);
+    myTodoList.add(entry);
+    appendEntryHtml(entry, "myTodoList");
 
     handleChangedTodoListData()
 }
 
+function initStartupTodoListEntries() {
+
+}
+
 function deleteEntryById(id) {
-    todoList.removeEntryById(id);
+    myTodoList.removeEntryById(id);
     removeEntryHtmlById(id);
 
     handleChangedTodoListData()
 }
 
-function cleanupEntryHtml() {
-    $("#myTodoList").empty();
+function cleanupEntryHtml(todoListId) {
+    $("#" + todoListId).empty();
 }
 
 function removeEntryHtmlById(id) {
@@ -139,7 +146,7 @@ function removeEntryHtmlById(id) {
     $(idString).parents("li").remove();
 }
 
-function appendEntryHtml(entry) {
+function appendEntryHtml(entry, todoListId, simple = false) {
     let template = listEntryTemplate;
 
     template = template.replace(/{\$title}/g, entry.title);
@@ -153,7 +160,16 @@ function appendEntryHtml(entry) {
 
     template = template.replace(/{\$state}/g, checked);
 
-    $("#myTodoList").append($(template));
+    //insert arrows if not simple
+    if(!simple) {
+        template = template.replace(/{\$uparrowbutton}/g, upArrowButtonTemplate);
+        template = template.replace(/{\$downarrowbutton}/g, downArrowButtonTemplate);
+    } else {
+        template = template.replace(/{\$uparrowbutton}/g, "");
+        template = template.replace(/{\$downarrowbutton}/g, "");
+    }
+
+    $("#" + todoListId).append($(template));
 
     let htmlEntryCheckbox = $("#" + entry.id);
     let htmlListEntry = htmlEntryCheckbox.parents("li");
@@ -161,7 +177,7 @@ function appendEntryHtml(entry) {
         let input = $(this).find("input");
         let id = input.attr("id");
 
-        todoList.getEntryById(id).state = input.prop("checked");
+        myTodoList.getEntryById(id).state = input.prop("checked");
 
         handleChangedTodoListData();
     });
@@ -182,111 +198,15 @@ function appendEntryHtml(entry) {
 }
 
 function moveHtmlEntryUp(id) {
-    if(todoList.moveEntryUpById(id)) {
+    if(myTodoList.moveEntryUpById(id)) {
         handleChangedTodoListData();
-        loadEntriesFromStorage();
+        loadMyTodoListEntriesFromStorage();
     }
 }
 
 function moveHtmlEntryDown(id) {
-    if(todoList.moveEntryDownById(id)) {
+    if(myTodoList.moveEntryDownById(id)) {
         handleChangedTodoListData();
-        loadEntriesFromStorage();
-    }
-}
-
-
-function storeTodoListData() {
-    console.log("Storing TodoListData...");
-
-    let json = JSON.stringify(todoList);
-    localStorage.setItem("myTodo", json);
-}
-
-function loadTodoListData() {
-    let json = localStorage.getItem("myTodo");
-    let jsonObject = JSON.parse(json);
-    todoList = Object.assign(new TodoList, jsonObject);
-
-    if(todoList === null) {
-        todoList = new TodoList();
-    }
-}
-
-class TodoListEntry {
-    constructor(title, text, state) {
-        this.id = guid();
-        this.title = title;
-        this.text = text;
-        this.state = state;
-    }
-}
-
-class TodoList {
-    constructor() {
-        this.entries = []
-    }
-
-    add(entry) {
-        this.entries.push(entry);
-    }
-
-    removeEntryById(id) {
-        const result = this.entries.filter(e => e.id === id);
-
-        if(result.length > 0) {
-            let i = this.entries.indexOf(result[0]);
-
-            if(i !== -1) {
-                this.entries.splice(i, 1);
-            }
-        }
-    }
-
-    getEntryById(id) {
-        return this.entries.filter(e => e.id === id)[0];
-    }
-
-    moveEntryUpById(id) {
-        let entry = this.getEntryById(id);
-        if(entry === undefined) return false;
-
-        let pos = this.entries.indexOf(entry);
-        let newPos = pos-1;
-
-        if(newPos >= 0) {
-            TodoList.array_move(this.entries, pos, newPos);
-
-            return true;
-        }
-
-        return false;
-    }
-
-    moveEntryDownById(id) {
-        let entry = this.getEntryById(id);
-        if(entry === undefined) return false;
-
-        let pos = this.entries.indexOf(entry);
-        let newPos = pos+1;
-
-        if(newPos < this.entries.length) {
-            TodoList.array_move(this.entries, pos, newPos);
-
-            return true;
-        }
-
-        return false;
-    }
-
-    static array_move(arr, old_index, new_index) {
-        if (new_index >= arr.length) {
-            let k = new_index - arr.length + 1;
-            while (k--) {
-                arr.push(undefined);
-            }
-        }
-        arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-        return arr;
+        loadMyTodoListEntriesFromStorage();
     }
 }
