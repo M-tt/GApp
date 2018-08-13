4f973ad427d24c2f3b2e5d895ac064e7afe9132b
René Ziegler <ziegler.rene87@gmx.de>
2018-08-13 15:33:47 +0200

    Fixed some typos Added texts for "Einzelunternehmen" and "GbR"

--- js/questionnaireHandler.js
+++ js/questionnaireHandler.js
@@ -225,41 +225,36 @@ answers.push(new Answer("aTeamPartGY", "rPartG", "Ja"));
 answers.push(new Answer("aTeamPartGN", "rGbR", "Nein"));
 
 //=================RESULTS=================//
-results.push(new Result("rEinzelunternehmen", "Einzelunternehmen", "done", ""));
+results.push(new Result("rEinzelunternehmen", "Einzelunternehmen", "done",
+    "<p>Du könntest dich als Kleingewerbetreibender oder als Kaufmann e.K. selbstständig machen. Sowohl als Kleingewerbetreibender als auch als Kaufmann e.K. musst du dein Gewerbe beim Gewerbeamt und dem Finanzamt anmelden. In beiden Fällen ist kein Mindeststartkapital vorgeschrieben. Obwohl grundsätzlich die Gewerbesteuer abgeführt werden muss, gilt für Kleingewerbetreibende und Kaufleute e.K. ein Steuerfreibetrag von 24.500 € jährlich. überschreitet der Umsatz im Gründungsjahr 17.500 € bzw. im Folgejahr 50.000 € nicht, gilt zudem die Kleinunternehmerregelung. In diesem Fall kann auf die Erhebung und Abführung der Umsatzsteuer verzichtet werden. Je nach ausgeübter Tätigkeit kann eine Mitgliedschaft bei der IHK oder der HWK Pflicht sein.</p>" +
+    "<p>Der Vorteil eines Kleingewerbes ist, dass keine Handelsregistereintragung vorgenommen werden muss. Die Buchhaltung kann in der Form der Einnahmen überschuss Rechnung (EÜR) erfolgen. Nimmst du mit deinem Kleingewerbe weniger als 8.820 € ein, musst du keine Einkommenssteuer zahlen. Die Gründungskosten sind mit 50 € gering. Die Gründungsdauer ist mit einem Tag sehr kurz.</p>" +
+    "<p>Bei einer Selbstständigkeit als Kaufmann e.K. muss ein Handelsregistereintrag vorgenommen werden. Anders als bei einem Kleingewerbe ist es dafür aber möglich, einen Fantasienamen als Firmennamen zu verwenden. Die Buchhaltung eines Kaufmann e.K. muss je nachdem, ob Schwellenwerte überschritten werden oder nicht, über die doppelte Buchführung / Bilanzierung oder die EÜR erfolgen. Die Gründungskosten betragen ca. 300 €. Die Gründungsdauer beträgt ca. 2 Wochen.</p>"));
 
 
-results.push(new Result("rUG", "UG (haftungsbeschränkt)", "done", "<p>Die Unternehmergesellschaft (haftungsbeschränkt), auch Mini-GmbH oder Ein-Euro-GmbH genannt, ist eine Kapitalgesellschaft. Das bedeutet, dass die Haftung, wie bei der GmbH, auf das Firmenvermögen beschränkt ist. Auch bei der UG (haftungsbeschränkt) ist ein Gesellschaftsvertrag zu unterzeichnen und der Notar übernimmt die Eintragung in das Handelsregister. Die Kosten können durch die Verwendung eines beurkundungspflichtigen Musterprotokolls reduziert werden. </p><p> Anders als bei der GmbH kann die UG (haftungsbeschränkt) bereits mit einem Euro Start- bzw. Stammkapital gegründet werden.\n" +
-    "Allerdings empfiehlt es sich in der Praxis auch für die UG (haftungsbeschränkt) mehr Kapital einzusetzen. Die UG (haftungsbeschränkt) ist verpflichtet, Rücklagen in Höhe von mindestens 25 % des Jahresüberschusses zu bilden. Dies gilt solange, bis ein Mindeststammkapital von 25.000 € erreicht ist. Die Buchführung muss nach dem HGB erfolgen (doppelte Buchführung / Bilanzierung).</p>"));
+results.push(new Result("rUG", "UG (haftungsbeschränkt)", "done",
+    "<p>Die Unternehmergesellschaft (haftungsbeschränkt), auch Mini-GmbH oder Ein-Euro-GmbH genannt, ist eine Kapitalgesellschaft. Das bedeutet, dass die Haftung, wie bei der GmbH, auf das Firmenvermögen beschränkt ist. Auch bei der UG (haftungsbeschränkt) ist ein Gesellschaftsvertrag zu unterzeichnen und der Notar übernimmt die Eintragung in das Handelsregister. Die Kosten können durch die Verwendung eines beurkundungspflichtigen Musterprotokolls reduziert werden.</p>" +
+    "<p> Anders als bei der GmbH kann die UG (haftungsbeschränkt) bereits mit einem Euro Start- bzw. Stammkapital gegründet werden. Allerdings empfiehlt es sich in der Praxis auch für die UG (haftungsbeschränkt) mehr Kapital einzusetzen. Die UG (haftungsbeschränkt) ist verpflichtet, Rücklagen in Höhe von mindestens 25 % des Jahresüberschusses zu bilden. Dies gilt solange, bis ein Mindeststammkapital von 25.000 € erreicht ist. Die Buchführung muss nach dem HGB erfolgen (doppelte Buchführung / Bilanzierung).</p>"));
 
 
-results.push(new Result("rGmbH", "GmbH", "done", "<p>" +
-    "Die GmbH ist eine sog. Kapitalgesellschaft, d.h. die Haftung ist auf das Firmenvermögen beschränkt." +
-    "Um eine GmbH gründen zu können, muss ein schriftlicher, beglaubigter Gesellschaftsvertrag unterzeichnet werden." +
-    "Die Gesellschafterversammlung ist das oberste Organ der GmbH. Diese fasst Beschlüsse anhand von Stimmrechten" +
-    "auf Basis der jeweiligen Anteile der Gesellschafter an der GmbH." +
-    "</p>" +
-    "<p>" +
-    "Die GmbH wird durch einen oder mehrere Geschäftsführer nach außen vertreten. Die Geschäftsführer werden von den Gesellschaftern ernannt und unterliegen deren Weisungen." +
-    "</p>" +
-    "<p>" +
-    "Die GmbH muss im Handelsregister eingetragen und im Gewerbeamt angemeldet werden." +
-    "Die Buchführung muss nach dem HGB erfolgen (doppelte Buchführung / Bilanzierung)." +
-    "</p>"));
+results.push(new Result("rGmbH", "GmbH", "done",
+    "<p>Die GmbH ist eine sog. Kapitalgesellschaft, d.h. die Haftung ist auf das Firmenvermögen beschränkt. Um eine GmbH gründen zu können, muss ein schriftlicher, beglaubigter Gesellschaftsvertrag unterzeichnet werden. Die Gesellschafterversammlung ist das oberste Organ der GmbH. Diese fasst Beschlüsse anhand von Stimmrechten auf Basis der jeweiligen Anteile der Gesellschafter an der GmbH.</p>" +
+    "<p>Die GmbH wird durch einen oder mehrere Geschäftsführer nach außen vertreten. Die Geschäftsführer werden von den Gesellschaftern ernannt und unterliegen deren Weisungen.</p>" +
+    "<p>Die GmbH muss im Handelsregister eingetragen und im Gewerbeamt angemeldet werden. Die Buchführung muss nach dem HGB erfolgen (doppelte Buchführung / Bilanzierung).</p>"));
 
 
-results.push(new Result("rFreiberufler", "Freiberufler", "done", "<p>Als Freiberufler kannst du von einigen Vorteilen profitieren." +
-    "Zum einen sind Freiberufler von der Gewerbesteuer befreit. Die Umsatzsteuer ist nur abzuführen," +
-    "wenn dein Unternehmen mehr als 17.500 € Jahresumsatz generiert.</p>" +
-    "<p>Die Anmeldung deiner Tätigkeit solltest du innerhalb von vier Wochen nach Aufnahme deines Gewerbes" +
-    "beim Finanzamt einreichen. Das Finanzamt entscheidet auch darüber, ob du als Freiberufler oder Gewerbetreibender geführt wirst.</p>" +
-    "<p>Um deine Versicherungen solltest du dich natürlich auch kümmern. Es ist z.B. möglich," +
-    "sich über die Künstlersozialkasse zu versichern, oder in die gesetzliche bzw. die private Krankenversicherung einzuzahlen.</p>"));
+results.push(new Result("rFreiberufler", "Freiberufler", "done",
+    "<p>Als Freiberufler kannst du von einigen Vorteilen profitieren. Zum einen sind Freiberufler von der Gewerbesteuer befreit. Die Umsatzsteuer ist nur abzuführen, wenn dein Unternehmen mehr als 17.500 € Jahresumsatz generiert.</p>" +
+    "<p>Die Anmeldung deiner Tätigkeit solltest du innerhalb von vier Wochen nach Aufnahme deines Gewerbes beim Finanzamt einreichen. Das Finanzamt entscheidet auch darüber, ob du als Freiberufler oder Gewerbetreibender geführt wirst.</p>" +
+    "<p>Um deine Versicherungen solltest du dich natürlich auch kümmern. Es ist z.B. möglich, sich über die Künstlersozialkasse zu versichern, oder in die gesetzliche bzw. die private Krankenversicherung einzuzahlen.</p>"));
 
 
 results.push(new Result("rPartG", "Partnerschaftsgesellschaft", "done", ""));
 
 
-results.push(new Result("rGbR", "Gesellschaft bürgerlichen Rechts", "done", ""));
+results.push(new Result("rGbR", "Gesellschaft bürgerlichen Rechts", "done",
+    "<p>Eine GbR kann von mindestens zwei Gesellschaftern gegründet werden. Grundsätzlich sind die Gesellschafter gemeinsam zur Geschäftsführung berechtigt und haften zu gleichen Teilen für die Verbindlichkeiten der Gesellschaft. Im Gesellschaftsvertrag, der grundsätzlich formfrei geschlossen werden kann, können individuelle Regelungen z.B. zu der Geschäftsführung und der Haftung beschlossen werden. Der Geschäftszweck kann jede erlaubte, gewerbliche Tätigkeit sein.</p>" +
+    "<p>Für die Gründung einer GbR ist kein Mindeststartkapital nötig. Die GbR muss bei dem Gewerbeamt, dem Finanzamt und der Berufsgenossenschaft angemeldet werden. Je nach ausgeübter Tätigkeit ist eine Mitgliedschaft bei der IHK oder der HWK Pflicht. Die Buchführung muss je nachdem, ob Schwellenwerte überschritten werden oder nicht, über die doppelte Buchführung / Bilanzierung oder die Einnahmen Überschuss Rechnung erfolgen.</p>" +
+    "<p>Nachteil einer GbR ist, dass kein Fantasiename vergeben werden darf. Des Weiteren ist diese Rechtsform nicht für Investoren geeignet ist. Die Umwandlung z.B. in eine GmbH ist aber v.a. bei bestehenden Geschäftstätigkeiten zeitaufwändig und komplex. Zudem wandelt sich die GbR bei hohen Umsätzen oder komplexerer Geschäftstätigkeit automatisch in eine OHG um. Dies ist mit einer umfangreicheren Buchführung und dem Eintrag ins Handelsregister verbunden.</p>"));
 
 
 questions.forEach(q => q.fetchAnswers());
